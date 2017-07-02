2188751c351756f850d38e3ff9fd871bd5aec671
nanjizal <nitsuj@nanjizal.net>
2017-07-02 11:55:35 +0100

    Add files via upload

---
+++ justDrawing/SurfaceJPanel.hx
@@ -0,0 +1,23 @@
+package justDrawing;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.javax.swing.JPanel;
+
+class SurfaceJPanel extends JPanel
+{
+    public var g: Graphics2D;
+    public var paintFunction: Graphics2D -> Void;
+    public function new(){ super( true ); }
+
+    @:overload override public function paintComponent( g2: Graphics ){
+        super.paintComponent( g2 );
+        var g2D: Graphics2D = cast g2;
+        var rHint = RenderingHints;
+        g2D.setRenderingHint( rHint.KEY_ANTIALIASING, rHint.VALUE_ANTIALIAS_ON );
+        g2D.setRenderingHint( rHint.KEY_RENDERING, rHint.VALUE_RENDER_QUALITY );
+        if (this.paintFunction != null) paintFunction( g2D );
+        g2D.dispose();
+    }
+}
